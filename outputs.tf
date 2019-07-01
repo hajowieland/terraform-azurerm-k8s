c93479e578395ed09f5b0aa4b8f357b58d1ff82b
hajowieland <mail@wieland.tech>
2019-07-01 23:34:01 +0200

    add public IP address and FQDN to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "kubeconfig_path_aks" {
   value = "${local_file.kubeconfigaks.0.filename}"
 }
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.*.ip_address
+}
+
+output "public_ip_fqdn" {
+  value = azurerm_public_ip.public_ip.*.fqdn
+}
