c486d508216ea35605410552d31ce9592dff1481
hajowieland <mail@wieland.tech>
2019-07-02 01:57:42 +0200

    add latest_k8s_version output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "kubeconfig_path_aks" {
   value = "${local_file.kubeconfigaks.0.filename}"
 }
 
+output "latest_k8s_version" {
+  value = data.azurerm_kubernetes_service_versions.current.latest_version
+}
+
+
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.*.ip_address
 }
